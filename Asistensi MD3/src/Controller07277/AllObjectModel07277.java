20748f35879c9a8aab4712a6adf39b3c1ee763bb
amaryan20 <94420026+Azhar170702@users.noreply.github.com>
2022-01-05 23:02:56 +0700

    pendataan_vaksinasi07277

---
+++ Asistensi MD3/src/Controller07277/AllObjectModel07277.java
@@ -0,0 +1,9 @@
+package Controller07277;
+
+import Model07277.AdminModel0727707277;
+import Model07277.PendudukModel07277;
+
+public class AllObjectModel07277 {
+    public static AdminModel0727707277 adminModel07277 = new AdminModel0727707277();
+    public static PendudukModel07277 pendudukModel = new PendudukModel07277();
+}
